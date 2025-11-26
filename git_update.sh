5f5af0d9329f43fdcd25b724b16e867fac8bcfc5
zhuli <18260036161@163.com>
2025-11-26 16:57:02 +0800

    2025.11.26

---
+++ git_update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# ===============================================
+# GIT 代码更新
+# 使用方法：
+#   ./git_update.sh commit_description    
+# ===============================================
+
+COMMIT_DESCRIPTION=${1:-1}
+git add .
+git commit -m "$COMMIT_DESCRIPTION"
+git push -u origin main
